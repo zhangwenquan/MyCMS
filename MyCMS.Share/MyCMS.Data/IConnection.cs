0b095f9160953c561c48687808c265a1548ea98d
zhangwenquan <zwq_343@163.com>
2014-10-13 17:59:07 +0800

    add classDiagram

---
+++ MyCMS.Share/MyCMS.Data/IConnection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyCMS.Data
+{
+    public interface IConnection
+    {
+        IDbDriver Driver
+        {
+            get;
+            set;
+        }
+
+        bool IsTransaction
+        {
+            get;
+            set;
+        }
+
+        SqlStatement Query();
+
+        SqlStatement QueryScalar();
+
+        SqlStatement Update();
+
+        void Commit();
+
+        void Rollback();
+
+        string TableExists();
+    }
+}
---
+++ MyCMS.Share/MyCMS.Data/SqlStatement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyCMS.Data
+{
+    public class SqlStatement
+    {
+        public System.Data.CommandType Type
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string SqlClause
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public List<DataParameter> Parameters
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
